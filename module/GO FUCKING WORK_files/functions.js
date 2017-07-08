d0c2f9a1f1860ae2d11176847d045375144ff028
Ted Lin <mint@mail.ustc.edu.cn>
2017-07-08 16:33:31 +0800

    add next & preview with hammer.js

--- module/GO FUCKING WORK_files/functions.js
+++ module/GO FUCKING WORK_files/functions.js
@@ -3333,19 +3333,45 @@
   ];
 
 
-var min = 0;
-var max = textCotents.length-1;
-randomIndex = Math.floor(Math.random() * (max - min + 1)) + min;
 
 function getQuote (callback){
 	    callback(textCotents[randomIndex]);
 }
 
-function getTextTag() {
-return textCotents[randomIndex];
+function getTextTag(index) {
+return textCotents[index];
+}
+
+function getImageTag(index) {
+return imageURLs[index];
 }
 
 
-function getImageTag() {
-return imageURLs[randomIndex];
+// https://stackoverflow.com/questions/14573223/set-cookie-and-get-cookie-with-javascript
+
+function createCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
 }
+
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+function eraseCookie(name) {
+    createCookie(name,"",-1);
+}
+
+
